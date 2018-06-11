90ab8abac20fd9aa34b55ed0a5320d221a35f8b0
Yaseen AlDallash <yaseenmeald@hotmail.com>
2018-06-11 14:37:39 -0400

    added tests

--- Question 1.playground/Contents.swift
+++ Question 1.playground/Contents.swift
@@ -1,10 +1,11 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import XCTest
 
 //Question 1 -- sortByStrings(s,t): Sort the letters in the string s by the order they occur in the string t. You can assume t will not have repetitive characters. For s = "weather" and t = "therapyw", the output should be sortByString(s, t) = "theeraw". For s = "good" and t = "odg", the output should be sortByString(s, t) = "oodg".
 
-let testCaseOne = (s: "weather", t: "therapyw", answer: "theeraw")
+
 func sortByStrings(_ s: String, _ t: String)->String{
     /*Since I can assume t will not have repetitive characters.
      - I can build a dictionary with it's characters where keys are the characters and values are their positions
@@ -17,4 +18,18 @@ func sortByStrings(_ s: String, _ t: String)->String{
     }
     return String(s.sorted{ myDictionary[$0] ?? 0 < myDictionary[$1] ?? 0})
 }
-sortByStrings(testCaseOne.s, testCaseOne.t)
+
+class Examples: XCTestCase {
+    static public func testExampleOne(){
+        let testCaseOne = (s: "weather", t: "therapyw", answer: "theeraw")
+        let testResult = sortByStrings(testCaseOne.s, testCaseOne.t)
+        XCTAssertEqual(testResult, testCaseOne.answer, "\(testResult) is no equal to \(testCaseOne.answer)")
+    }
+    static public func testExampleTwo(){
+        let testCaseTwo = (s: "good", t: "odg", answer: "oodg")
+        let testResult = sortByStrings(testCaseTwo.s, testCaseTwo.t)
+        XCTAssertEqual(testResult, testCaseTwo.answer, "\(testResult) is no equal to \(testCaseTwo.answer)")    }
+}
+Examples.testExampleOne()
+
+
